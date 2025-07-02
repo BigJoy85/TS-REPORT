767fb2270e62d0d9264140cb416062504df7d451
Rizky Dharma Pramana <ads.prime85@gmail.com>
2025-07-02 17:57:52 +0700

    Update models.py

--- api/models.py
+++ api/models.py
@@ -8,6 +8,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.String(50), nullable=False, default='user')  # ✅ Kolom wajib
     laporan = db.relationship('Laporan', backref='user', lazy=True)
 
 class Laporan(db.Model):
